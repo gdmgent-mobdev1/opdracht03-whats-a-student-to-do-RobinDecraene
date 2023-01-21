2ffdc562cddfbc65ace4e74a6142ac4450738ec0
Decraene Robin <robin.decraene@student.arteveldehs.be>
2023-01-21 14:11:12 +0100

    commit

--- src/lib/ProjectDetailScherm.ts
+++ src/lib/ProjectDetailScherm.ts
@@ -1,27 +1,31 @@
 import { MyApp } from "../app";
 import { Scherm } from "./scherm";
+import { FireBase } from "./Firebase";
 
 export class ProjectDetailScherm extends Scherm {
     private arrowTodo : HTMLImageElement;
-    private hour;
-    private minutes;
-    private seconds;
+    // private hour;
+    // private minutes;
+    // private seconds;
     private timerBtn : HTMLButtonElement;
-    private timer;
-    private totaltime;
-    private timerId : HTMLElement;
+    // private timer;
+    // private totaltime;
+    // private timerId : HTMLElement;
+    private name!: string;
+    private nameProject : HTMLElement;
 
     constructor(app: MyApp){
       super("projectDetailsScherm", app);
       this.arrowTodo = document.getElementById("arrowTodo") as HTMLImageElement;
       this.timerBtn = document.getElementById("timerBtn") as HTMLButtonElement;
-      this.timer = setInterval(countTimer, 1000);
-      this.totaltime = 0;
-      this.hour = Math.floor(this.totaltime /3600);
-      this.minutes = Math.floor((this.totaltime - this.hour*3600)/60);
-      this.seconds = this.totaltime - (this.hour*3600 + this.minutes*60);
-      this.timerId = document.getElementById("timerId") as HTMLElement;
-   }
+      // this.timer = setInterval(countTimer, 1000);
+      // this.totaltime = 0;
+      // this.hour = Math.floor(this.totaltime /3600);
+      // this.minutes = Math.floor((this.totaltime - this.hour*3600)/60);
+      // this.seconds = this.totaltime - (this.hour*3600 + this.minutes*60);
+      // this.timerId = document.getElementById("timerId") as HTMLElement;
+      this.nameProject = document.getElementById("nameProject") as HTMLElement;
+    }
 
   public init(){
     this.arrowTodo.onclick = () => {
@@ -29,18 +33,29 @@ export class ProjectDetailScherm extends Scherm {
       this.app.homeScherm.show();
     }
 
-    this.timerBtn.onclick = () => {
-      countTimer();
-    }
+    // this.timerBtn.onclick = () => {
+    //   countTimer();
+    // }
   }
+
+  public open(room: string) {
+    this.name = room;
+    this.show();
 }
 
-function countTimer(this: any): void {
-  ++this.totaltime;
-  this.hour;
-  this.minutes;
-  this.seconds;
-  this.timerId.innerHTML = this.hour + ":" + this.minutes + ":" + this.seconds;
+public show() {
+  this.nameProject.innerHTML = this.name;
+  const test = document.createElement('h3');
+  test.innerHTML = 'hallo';
 }
+}
+
+// function countTimer(this: any) {
+//   ++this.totaltime;
+//   this.hour;
+//   this.minutes;
+//   this.seconds;
+//   this.timerId.innerHTML = this.hour + ":" + this.minutes + ":" + this.seconds;
+// }
 
 
--- src/lib/homeScherm.ts
+++ src/lib/homeScherm.ts
@@ -5,16 +5,39 @@ export class HomeScherm extends Scherm {
   private addTodoListInput : HTMLInputElement;
   private addTodoListButton : HTMLButtonElement;
   private accountImg : HTMLImageElement;
-  private projectCard : HTMLElement;
+  // private projectCard : HTMLElement;
   private add : HTMLImageElement;
+  private projects : HTMLElement;
+  public projectRooms = [
+    'Project mob dev', 'Taak UX', 'Powerpoint PPD', 'Taak web dev'
+  ]
 
   constructor(app: MyApp) {
     super("homeScherm", app);
     this.addTodoListInput = document.getElementById("addTodoListInput") as HTMLInputElement;
     this.addTodoListButton = document.getElementById("addTodoListButton") as HTMLButtonElement;
     this.accountImg = document.getElementById("accountImg") as HTMLImageElement;
-    this.projectCard = document.getElementById("projectCard") as HTMLElement;
+    // this.projectCard = document.getElementById("projectCard") as HTMLElement;
     this.add = document.getElementById("add") as HTMLImageElement;
+    this.projects = document.getElementById("projects") as HTMLElement;
+  }
+
+  private createProject(name: string) : HTMLElement {
+    const projectRoom = document.createElement('div');
+    projectRoom.setAttribute('id', 'projectCard');
+    const img = document.createElement('img');
+    img.src = "./src/img/project.jpg";
+    const div = document.createElement('div');
+    const title = document.createElement('h3');
+    title.innerHTML = name;
+
+    div.append(title);
+    projectRoom.append(img, div);
+    projectRoom.onclick = () => {
+      this.hide();
+      this.app.projectDetailScherm.open(name);
+    }
+    return projectRoom;
   }
 
   public init(){
@@ -29,9 +52,9 @@ export class HomeScherm extends Scherm {
       this.app.accountScherm.show();
     }
 
-    this.projectCard.onclick = () => {
-      this.hide();
-      this.app.projectDetailScherm.show();
-     }
+    this.projectRooms.forEach(room => {
+      const element = this.createProject(room);
+      this.projects.append(element);
+    })
   }
 }
